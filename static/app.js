a95b2b9889934f39ec11f466aef96d40c8c8d709
Kaelan Sun <kaelan@my.mail.de>
2022-06-09 15:48:30 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch('https://aiapchatbot.herokuapp/predict', {
+        fetch('/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
