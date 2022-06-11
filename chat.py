c2130ccf8d47fd52dc0f29d88c21256d82a51d12
Kaelan Sun <kaelan@my.mail.de>
2022-06-11 22:22:44 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ import torch
 
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
-from transformers import AutoModelForCausalLM, AutoTokenizer
+#from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # from gtts import gTTS
 # import os
@@ -30,8 +30,8 @@ model = NeuralNet(input_size, hidden_size, output_size)
 model.load_state_dict(model_state)
 model.eval()
 
-tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-small')
-gpt_model = AutoModelForCausalLM.from_pretrained('microsoft/DialoGPT-small')
+#tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-small')
+#gpt_model = AutoModelForCausalLM.from_pretrained('microsoft/DialoGPT-small')
 
 bot_name = "Jude"
 
@@ -53,28 +53,28 @@ def get_response(msg):
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
             
-    else:
-        #encoded_input = tokenizer(msg, return_tensors='pt')['input_ids']
-        new_user_input_ids = tokenizer.encode(msg + tokenizer.eos_token, return_tensors='pt')
-        step=0
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+#     else:
+#         #encoded_input = tokenizer(msg, return_tensors='pt')['input_ids']
+#         new_user_input_ids = tokenizer.encode(msg + tokenizer.eos_token, return_tensors='pt')
+#         step=0
+#         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
 
-        chat_history_ids = gpt_model.generate(
-               bot_input_ids, max_length=500,
-               pad_token_id=tokenizer.eos_token_id,
-               no_repeat_ngram_size=3,
-               do_sample=True,
-               top_k=100,
-               top_p=0.7,
-               temperature=0.8
-            )
+#         chat_history_ids = gpt_model.generate(
+#                bot_input_ids, max_length=500,
+#                pad_token_id=tokenizer.eos_token_id,
+#                no_repeat_ngram_size=3,
+#                do_sample=True,
+#                top_k=100,
+#                top_p=0.7,
+#                temperature=0.8
+#             )
 
-        #print("AI: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
-        output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        #output = model(**encoded_input)
-        return output   
+#         #print("AI: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+#         output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+#         #output = model(**encoded_input)
+#         return output   
 
-    #return "I do not understand..."
+    return "I do not understand..."
 
 
 # def gtts_speech(response):
