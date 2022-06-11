12db41cc509a81df4d75e08104f95e42356a7186
Kaelan Sun <kaelan@my.mail.de>
2022-06-11 12:50:49 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -53,26 +53,26 @@ def get_response(msg):
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
             
-     else:
-        #encoded_input = tokenizer(msg, return_tensors='pt')['input_ids']
-        new_user_input_ids = tokenizer.encode(msg + tokenizer.eos_token, return_tensors='pt')
-        step=0
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+    else:
+       #encoded_input = tokenizer(msg, return_tensors='pt')['input_ids']
+       new_user_input_ids = tokenizer.encode(msg + tokenizer.eos_token, return_tensors='pt')
+       step=0
+       bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
 
-        chat_history_ids = gpt_model.generate(
-            bot_input_ids, max_length=500,
-            pad_token_id=tokenizer.eos_token_id,
-            no_repeat_ngram_size=3,
-            do_sample=True,
-            top_k=100,
-            top_p=0.7,
-            temperature=0.8
-        )
+       chat_history_ids = gpt_model.generate(
+           bot_input_ids, max_length=500,
+           pad_token_id=tokenizer.eos_token_id,
+           no_repeat_ngram_size=3,
+           do_sample=True,
+           top_k=100,
+           top_p=0.7,
+           temperature=0.8
+       )
 
-        #print("AI: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
-        output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        #output = model(**encoded_input)
-        return output   
+       #print("AI: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+       output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+       #output = model(**encoded_input)
+       return output   
 
     #return "I do not understand..."
 
