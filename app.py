30c4c130fb38e1a5240440c654a5ae40e68e209b
Kaelan Sun <kaelan@my.mail.de>
2022-06-12 01:09:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ language = 'en'
 
 
 def gtts_speech(response):
-    myobj = gTTS(text=response, lang=language, slow=True, tld='com.sg')
-    myobj.save("./welcome.mp3")
+    myobj = gTTS(text=response, lang=language, slow=False, tld='com.sg')
+    myobj.save("welcome.mp3")
     #os.system("mpg321 ./welcome.mp3")
-    os.system("./welcome.mp3")
-    os.remove("./welcome.mp3")
+    os.system("welcome.mp3")
+    os.remove("welcome.mp3")
     #mixer.init()
     #mixer.music.load('welcome.mp3')# load the audio file
     #mixer.music.play()
