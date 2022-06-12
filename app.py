d6ba1e917b862ac5194f7583154f8b822a28bd9f
Kaelan Sun <kaelan@my.mail.de>
2022-06-12 17:23:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
-from chatblend import get_response
+from chat import get_response
 from gtts import gTTS
 #import playsound
 #from pygame import mixer
